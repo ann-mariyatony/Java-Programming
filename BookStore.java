e5b369a39ab1cd17875632b09cad40eb465d4ea6
ann-mariyatony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-07-30 10:51:16 +0530

    Update BookStore.java

--- BookStore.java
+++ BookStore.java
@@ -1,5 +1,33 @@
 package ann;
-
+class Book{
+	String title;
+	String author;
+	double price;
+	int quantity;
+	double totalValue;
+	Book(String title,String author,double price,int quantity){
+		this.title=title;
+		this.author=author;
+		this.price=price;
+		this.quantity=quantity;
+	}
+	public void calaculateTotalValue() {
+		totalValue=(price*quantity);
+	}
+	public void displayDetails() {
+		System.out.println("Title:"+ title);
+		System.out.println("Author:"+ author);
+		System.out.println("Price:"+price);
+		System.out.println("Quantity:"+quantity);
+		System.out.println("Total Value:"+ totalValue);
+}
 public class BookStore {
+	public static void main(String[] args) {
+		 Book b1=new Book("dora","buji",209,3);
+		 b1.calaculateTotalValue();
+		 b1.displayDetails()
+		
+  }
+
 
 }
