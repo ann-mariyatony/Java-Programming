c6dc4f73e168fe6f51e97d53bf14842da89b7032
Ann Mariya Tony <annmariyatony2028@cs.sjcetpalai.ac.in>
2025-10-08 10:49:17 +0530

    Create GUIdemo.java

---
+++ GUIdemo.java
@@ -0,0 +1,39 @@
+import  javax.swing .*;
+import java.awt.*;
+public class guiademo1 {
+	
+	public static void main(String[] args)
+	{
+		 JFrame mainFrame = new JFrame ( );
+		 mainFrame.setSize(300, 150);
+		 mainFrame.setTitle("My First GUI");
+		
+		 JPanel panel = new JPanel();
+		 panel.setLayout(new GridLayout(3,2,10,10));
+		 
+		 JLabel userLabel = new JLabel("User Name");
+		 JTextField userField = new JTextField();
+		 
+		 JLabel pwdLabel = new JLabel("Password");
+		 JPasswordField pwdField = new JPasswordField();
+		 
+		 JButton loginbtn = new JButton("Login");
+		 
+		 panel.add(userLabel);
+		 panel.add(userField);
+		 panel.add(pwdLabel);
+		 panel.add(pwdField);
+		 panel.add(new JLabel(" ") );
+		 panel.add(loginbtn);
+		 
+		 
+	mainFrame.add(panel);
+		 
+		mainFrame.setLocationRelativeTo(null); //Output will be displayed to the center of screen
+		 
+		 
+		 
+		 mainFrame.setVisible(true);
+	}
+	}
+
